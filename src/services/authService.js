1eafd7869b18363578e6dbcd6c0276ddf4cb1bdc
TalhaZaheer1 <tzaheer72@gmail.com>
2025-06-20 02:14:14 -0700

    changes

--- src/services/authService.js
+++
@@ -1,6 +0,0 @@
-import api from './api';
-
-export const loginAdmin = async (email, password) => {
-  const res = await api.post('/auth/login', { email, password });
-  return res.data;
-};
--- src/services/ttsService.js
+++
@@ -1,5 +0,0 @@
-// ttsService.js
-export const getTTSUrl = async (text) => {
-  const res = await fetch(`http://localhost:5000/api/tts?text=${encodeURIComponent(text)}`);
-  return await res.json();
-};
